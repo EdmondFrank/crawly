0dda64dfe6c855c2433e5adb513e363eba277153
EdmondFrank <Edmondfrank@hotmail.com>
2021-05-13 10:23:48 +0800

    Feat support ignore list for bloom filter

--- lib/crawly/middlewares/bloom_unique_request.ex
+++ lib/crawly/middlewares/bloom_unique_request.ex
@@ -10,20 +10,25 @@ defmodule Crawly.Middlewares.BloomUniqueRequest do
 
     capacity = Map.get(opts, :capacity, 1_000_000)
     false_positive = Map.get(opts, :false_positive, 0.001)
+    ignore_list = Map.get(opts, :ignore_list, [])
 
-    unique_request_bloom_filter =
-      Map.get(state, :unique_request_bloom_filter, init_bloom(capacity, false_positive))
-
-    if :bloom.check(unique_request_bloom_filter, request.url) do
-      Logger.debug(
-        "Dropping request by bloom filter: #{request.url}, as it's already processed"
-      )
-
-      {false, state}
-    else
-      :bloom.set(unique_request_bloom_filter, request.url)
-
+    if request.url in ignore_list do
       {request, state}
+    else
+      unique_request_bloom_filter =
+        Map.get(state, :unique_request_bloom_filter, init_bloom(capacity, false_positive))
+
+      if :bloom.check(unique_request_bloom_filter, request.url) do
+        Logger.debug(
+          "Dropping request by bloom filter: #{request.url}, as it's already processed"
+        )
+
+        {false, state}
+      else
+        :bloom.set(unique_request_bloom_filter, request.url)
+
+        {request, state}
+      end
     end
   end
 
